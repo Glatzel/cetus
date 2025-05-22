5d3b891f2ec0f9d6ce86982c4c2b396e70a5a98c
Glatzel <893016099@qq.com>
2025-05-22 20:04:19 +0800

    build: remove Delete the Microsoft repository keys file (#4)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- image/common/install-deps.sh
+++ image/common/install-deps.sh
@@ -9,13 +9,12 @@ apt-get install -y --no-install-recommends \
     curl \
     git \
     libsqlite3-dev \
-    pkg-config \
-    unzip \
     wget
 
 # install pwsh
-wget -q https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb \
+wget -q https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb \
     && dpkg -i packages-microsoft-prod.deb \
+    && rm packages-microsoft-prod.deb \
     && apt-get update \
     && apt-get install -y powershell
 
