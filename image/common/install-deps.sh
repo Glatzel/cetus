74d3a32e46e97278652cb893f0624875743f9320
Glatzel <893016099@qq.com>
2025-05-22 19:36:37 +0800

    build: remove Delete the Microsoft repository keys file

--- image/common/install-deps.sh
+++ image/common/install-deps.sh
@@ -16,6 +16,7 @@ apt-get install -y --no-install-recommends \
 # install pwsh
 wget -q https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb \
     && dpkg -i packages-microsoft-prod.deb \
+    && rm packages-microsoft-prod.deb \
     && apt-get update \
     && apt-get install -y powershell
 
