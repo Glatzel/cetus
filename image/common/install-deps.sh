b7234196e758dad6065c26643031fcfe6c3c2f81
Glatzel <893016099@qq.com>
2025-10-01 11:20:37 +0800

    f

--- image/common/install-deps.sh
+++ image/common/install-deps.sh
@@ -11,12 +11,5 @@ apt-get install -y --no-install-recommends \
     libsqlite3-dev \
     wget
 
-# install pwsh
-wget -q https://packages.microsoft.com/config/ubuntu/24.04/packages-microsoft-prod.deb \
-    && dpkg -i packages-microsoft-prod.deb \
-    && rm packages-microsoft-prod.deb \
-    && apt-get update \
-    && apt-get install -y powershell
-
 apt-get clean
 dpkg --get-selections
