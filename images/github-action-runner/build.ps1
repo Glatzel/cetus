ff01185e7d885a91a6fe292324782e1e01fe4191
Glatzel <chenzelin9794@gmail.com>
2025-12-06 13:36:40 +0800

    f

--- images/github-action-runner/build.ps1
+++ images/github-action-runner/build.ps1
@@ -2,7 +2,9 @@ Set-Location $PSScriptRoot
 $name =Split-Path -Path $PSScriptRoot -Leaf
 $json = gh release view -R actions/runner --json tagName | ConvertFrom-Json
 $version = $json.tagName.Replace("v", "")
+$tag="Glatzel/cetus:${name}-${version}"
 docker build -f ./Dockerfile `
     --build-arg RUNNER_VERSION=$version `
-    -t Glatzel/cetus:${name}-${version} `
+    -t $tag `
     .
+"tag=$matrix" >> $env:GITHUB_OUTPUT
