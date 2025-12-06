43e92fd5a76c6d44057757c9d4d78d5adbccc884
Glatzel <chenzelin9794@gmail.com>
2025-12-06 13:23:33 +0800

    f

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -10,7 +10,7 @@ ForEach ($img in Get-ChildItem $PSScriptRoot/../images) {
 
 $matrix = $matrix |
 ConvertTo-Json -Depth 10 -Compress |
-jq '{include: .}'
+jq '{include: [.]}'
 
 # Clean CHANGED_KEYS
 $env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
