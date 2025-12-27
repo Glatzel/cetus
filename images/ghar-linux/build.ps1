4b37d864994bf8bad94aad57e46faf9b85db731d
Glatzel <893016099@qq.com>
2025-12-27 19:33:40 +0800

    Update build.ps1

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -1,6 +1,11 @@
 Set-Location $PSScriptRoot
 $name =Split-Path -Path $PSScriptRoot -Leaf
 $version = "0.0.1"
+docker buildx create `
+  --name multiarch `
+  --driver container `
+  --use
+docker buildx inspect --bootstrap
 docker buildx build -f ./Dockerfile `
     --platform linux/amd64,linux/arm64 `
     --build-arg RUNNER_VERSION=$version `
