009df43b75578d25c8da14dc3f87009cda1bbfa1
Glatzel <893016099@qq.com>
2025-12-27 16:32:25 +0800

    ci: Tag Docker images as latest before pushing (#132)
    Add tagging for Docker images as 'latest' before pushing to DockerHub
    and ghcr.
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- images/github-action-runner/build.ps1
+++ images/github-action-runner/build.ps1
@@ -2,9 +2,9 @@ Set-Location $PSScriptRoot
 $name =Split-Path -Path $PSScriptRoot -Leaf
 $json = gh release view -R actions/runner --json tagName | ConvertFrom-Json
 $version = $json.tagName.Replace("v", "")
-$tag="glatzel/${name}:${version}"
+$tag="${name}"
 docker build -f ./Dockerfile `
     --build-arg RUNNER_VERSION=$version `
     -t $tag `
     .
-"tag=$tag" >> $env:GITHUB_OUTPUT
+"version=$version" >> $env:GITHUB_OUTPUT
