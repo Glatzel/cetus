769bf9ec74d69ac6733f5f8c6be1afa87227c4a0
Glatzel <893016099@qq.com>
2025-12-27 19:57:25 +0800

    feat: add linux arm build (#134)

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -1,25 +1,27 @@
 $matrix = @()
-$machine_map = @{
-    'github-action-runner' = 'ubuntu-latest'
-'ghar-linux' = 'ubuntu-latest'
-}
-ForEach ($img in Get-ChildItem $PSScriptRoot/../images) {
-    $img=$img.Name
-    $matrix += [PSCustomObject]@{
-        image   = $img
-        machine = $machine_map["$img"]
+$csvData = Import-Csv "$PSScriptRoot/../images.csv"
+foreach ($row in $csvData) {
+    $img = $row.img
+    foreach ($machine in "windows-latest", "macos-latest", "ubuntu-latest", "ubuntu-24.04-arm") {
+        if ($row.$machine -eq "true") {
+            $matrix += [PSCustomObject]@{
+                img     = $img
+                machine = $machine
+            }
+        }
     }
 }
+
 
 $matrix = $matrix | ConvertTo-Json -Depth 10 -Compress | jq '{include: .}'
 # Clean CHANGED_KEYS
 $env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
 switch ($env:GITHUB_EVENT_NAME) {
     "push" {
-        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.image as $p | $images | index($p)))}'
+        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.img as $p | $images | index($p)))}'
     }
     "pull_request" {
-        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.image as $p | $images | index($p)))}'
+        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.img as $p | $images | index($p)))}'
     }
     default {
         $matrix = $matrix
