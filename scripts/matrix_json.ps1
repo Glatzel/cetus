ee9d38a061d684a3374447396a4244e29bb645f7
Glatzel <893016099@qq.com>
2025-12-27 17:58:56 +0800

    更新 matrix_json.ps1

--- scripts/matrix_json.ps1
+++ scripts/matrix_json.ps1
@@ -12,21 +12,20 @@ ForEach ($img in Get-ChildItem $PSScriptRoot/../images) {
 }
 
 write-output $matrix
-$matrix = $matrix | ConvertTo-Json -Depth 10 -Compress | jq '{include: [.]}'
-write-output $matrix
+#$matrix = $matrix | ConvertTo-Json -Depth 10 -Compress | jq '{include: [.]}'
 # Clean CHANGED_KEYS
-$env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
-switch ($env:GITHUB_EVENT_NAME) {
-    "push" {
-        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.image as $p | $images | index($p)))}'
-    }
-    "pull_request" {
-        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.image as $p | $images | index($p)))}'
-    }
-    default {
-        $matrix = $matrix
-    }
-}
+#$env:CHANGED_KEYS = "${env:CHANGED_KEYS}".Replace("\", "")
+#switch ($env:GITHUB_EVENT_NAME) {
+#    "push" {
+#        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.image as $p | $images | index($p)))}'
+#    }
+#    "pull_request" {
+#        $matrix = $matrix | jq -c --argjson images "${env:CHANGED_KEYS}" '{include: .include | map(select(.image as $p | $images | index($p)))}'
+#    }
+ #   default {
+ #       $matrix = $matrix
+#    }
+#}
 
 # Output matrix to GitHub Actions
 "matrix=$matrix" >> $env:GITHUB_OUTPUT
