f3c4684c6f49aa7663c447615820b39c26e53b2d
Glatzel <chenzelin9794@gmail.com>
2025-12-27 19:47:05 +0800

    发

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -1,13 +1,7 @@
 Set-Location $PSScriptRoot
 $name =Split-Path -Path $PSScriptRoot -Leaf
 $version = "0.0.1"
-docker buildx create `
-  --name multiarch `
-  --driver container `
-  --use
-docker buildx inspect --bootstrap
-docker buildx build -f ./Dockerfile `
-    --platform linux/amd64,linux/arm64 `
+docker build -f ./Dockerfile `
     --build-arg RUNNER_VERSION=$version `
     -t $name `
     .
