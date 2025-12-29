187838b506b9ec468a1021913dd2122b8ef65989
Glatzel <chenzelin9794@gmail.com>
2025-12-29 14:12:52 +0800

    f

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -9,7 +9,7 @@ $ROOT = git rev-parse --show-toplevel
 $json = gh release view -R actions/runner --json tagName | ConvertFrom-Json
 $runner_version = $json.tagName.Replace("v", "")
 
-docker buildx build -f ./Dockerfile `
+docker buildx build `
     --platform linux/amd64, linux/arm64 `
     --build-arg RUNNER_VERSION=$runner_version `
     --target local `
@@ -19,7 +19,7 @@ docker buildx build -f ./Dockerfile `
     -t ghcr.io/glatzel/ghar-linux-local:$runner_version `
     .
 $version = "0.0.1"
-docker buildx build -f ./Dockerfile `
+docker buildx build `
     --platform linux/amd64, linux/arm64 `
     --target cloud `
     -t glatzel/ghar-linux:latest `
