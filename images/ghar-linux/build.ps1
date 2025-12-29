4e86325323aa7bafad5bd4884ff433c4d98d9caf
Glatzel <chenzelin9794@gmail.com>
2025-12-29 14:38:32 +0800

    f

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -5,24 +5,46 @@ $ROOT = git rev-parse --show-toplevel
 $json = gh release view -R actions/runner --json tagName | ConvertFrom-Json
 $runner_version = $json.tagName.Replace("v", "")
 $version = "0.0.1"
-$pushFlag = if ($env:PUBLISH -eq "true") { "--push" } else { $null }
-docker buildx build `
-    $pushFlag `
-    --platform linux/amd64, linux/arm64 `
-    --build-arg RUNNER_VERSION=$runner_version `
-    --target local `
-    -t glatzel/ghar-linux-local:latest `
-    -t glatzel/ghar-linux-local:$runner_version `
-    -t ghcr.io/glatzel/ghar-linux-local:latest `
-    -t ghcr.io/glatzel/ghar-linux-local:$runner_version `
-    .
+if ($env:PUBLISH -eq "true") {
+    docker buildx build `
+        --push `
+        --platform linux/amd64, linux/arm64 `
+        --build-arg RUNNER_VERSION=$runner_version `
+        --target local `
+        -t glatzel/ghar-linux-local:latest `
+        -t glatzel/ghar-linux-local:$runner_version `
+        -t ghcr.io/glatzel/ghar-linux-local:latest `
+        -t ghcr.io/glatzel/ghar-linux-local:$runner_version `
+        .
 
-docker buildx build `
-    $pushFlag `
-    --platform linux/amd64, linux/arm64 `
-    --target cloud `
-    -t glatzel/ghar-linux:latest `
-    -t glatzel/ghar-linux:$version `
-    -t ghcr.io/glatzel/ghar-linux:latest `
-    -t ghcr.io/glatzel/ghar-linux:$version `
-    .
+    docker buildx build `
+        --push `
+        --platform linux/amd64, linux/arm64 `
+        --target cloud `
+        -t glatzel/ghar-linux:latest `
+        -t glatzel/ghar-linux:$version `
+        -t ghcr.io/glatzel/ghar-linux:latest `
+        -t ghcr.io/glatzel/ghar-linux:$version `
+        .
+}
+else {
+    docker buildx build `
+        --platform linux/amd64, linux/arm64 `
+        --build-arg RUNNER_VERSION=$runner_version `
+        --target local `
+        -t glatzel/ghar-linux-local:latest `
+        -t glatzel/ghar-linux-local:$runner_version `
+        -t ghcr.io/glatzel/ghar-linux-local:latest `
+        -t ghcr.io/glatzel/ghar-linux-local:$runner_version `
+        .
+
+    docker buildx build `
+        --platform linux/amd64, linux/arm64 `
+        --target cloud `
+        -t glatzel/ghar-linux:latest `
+        -t glatzel/ghar-linux:$version `
+        -t ghcr.io/glatzel/ghar-linux:latest `
+        -t ghcr.io/glatzel/ghar-linux:$version `
+        .
+
+}
