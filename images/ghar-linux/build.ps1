9edbeb295fecd95a57843351ad23baeac5417c47
Glatzel <chenzelin9794@gmail.com>
2025-12-30 12:44:57 +0800

    f

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -6,23 +6,34 @@ $json = gh release view -R actions/runner --json tagName | ConvertFrom-Json
 $runner_version = $json.tagName.Replace("v", "")
 $version = "0.0.1"
 $pushFlag = if ($env:PUBLISH -eq "true") { "--push" } else { $null }
+
 docker buildx build `
     $pushFlag `
-    --platform linux/amd64,linux/arm64 `
-    --build-arg RUNNER_VERSION=$runner_version `
-    --target local `
-    -t glatzel/ghar-linux-local:latest `
-    -t glatzel/ghar-linux-local:$runner_version `
-    -t ghcr.io/glatzel/ghar-linux-local:latest `
-    -t ghcr.io/glatzel/ghar-linux-local:$runner_version `
+    --platform linux/amd64, linux/arm64 `
+    --target release-cloud `
+    -t glatzel/ghar-linux-release-cloud:latest `
+    -t glatzel/ghar-linux-release-cloud:$version `
+    -t ghcr.io/glatzel/ghar-linux-release-cloud:latest `
+    -t ghcr.io/glatzel/ghar-linux-release-cloud:$version `
     .
 
 docker buildx build `
     $pushFlag `
-    --platform linux/amd64,linux/arm64 `
-    --target cloud `
-    -t glatzel/ghar-linux:latest `
-    -t glatzel/ghar-linux:$version `
-    -t ghcr.io/glatzel/ghar-linux:latest `
-    -t ghcr.io/glatzel/ghar-linux:$version `
+    --platform linux/amd64, linux/arm64 `
+    --build-arg RUNNER_VERSION=$runner_version `
+    --target dev-local `
+    -t glatzel/ghar-linux-dev-local:latest `
+    -t glatzel/ghar-linux-dev-local:$runner_version `
+    -t ghcr.io/glatzel/ghar-linux-dev-local:latest `
+    -t ghcr.io/glatzel/ghar-linux-dev-local:$runner_version `
+    .
+
+docker buildx build `
+    $pushFlag `
+    --platform linux/amd64, linux/arm64 `
+    --target release-local `
+    -t glatzel/ghar-linux-release-local:latest `
+    -t glatzel/ghar-linux-release-local:$version `
+    -t ghcr.io/glatzel/ghar-linux-release-local:latest `
+    -t ghcr.io/glatzel/ghar-linux-release-local:$version `
     .
