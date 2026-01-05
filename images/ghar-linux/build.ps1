d4bc71140e3ba65763f6c4ee176a6c5158ccc3c8
Glatzel <893016099@qq.com>
2026-01-05 18:46:18 +0800

    build: simplify image (#141)
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -5,6 +5,7 @@ $version = "0.0.3"
 $json = gh release view -R actions/runner --json tagName | ConvertFrom-Json
 $runner_version = $json.tagName.Replace("v", "")
 
+Write-Output "::group::release-cloud"
 docker buildx build `
     $pushFlag `
     --platform 'linux/amd64,linux/arm64' `
@@ -14,10 +15,12 @@ docker buildx build `
     -t ghcr.io/glatzel/ghar-linux-release-cloud:latest `
     -t ghcr.io/glatzel/ghar-linux-release-cloud:$version `
     .
-
+Write-Output "::endgroup::"
+Write-Output "::group::release-local"
 docker buildx build `
     $pushFlag `
     --platform 'linux/amd64,linux/arm64' `
+    --build-arg RUNNER_VERSION=$runner_version `
     --build-arg RUNNER_VERSION=$runner_version `
     --target release-local `
     -t glatzel/ghar-linux-release-local:latest `
@@ -25,3 +28,16 @@ docker buildx build `
     -t ghcr.io/glatzel/ghar-linux-release-local:latest `
     -t ghcr.io/glatzel/ghar-linux-release-local:$version `
     .
+Write-Output "::endgroup::"
+Write-Output "::group::dev-local"
+docker buildx build `
+    $pushFlag `
+    --platform 'linux/amd64,linux/arm64' `
+    --build-arg RUNNER_VERSION=$runner_version `
+    --target dev-local `
+    -t glatzel/ghar-linux-dev-local:latest `
+    -t glatzel/ghar-linux-dev-local:$runner_version `
+    -t ghcr.io/glatzel/ghar-linux-dev-local:latest `
+    -t ghcr.io/glatzel/ghar-linux-dev-local:$runner_version `
+    .
+Write-Output "::endgroup::"
---
+++ images/ghar-linux/download-action-runner.sh
@@ -0,0 +1,18 @@
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="x64"
+    RUNNER_URL="https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz";
+elif [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64" &&
+    RUNNER_URL="https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-arm64-${RUNNER_VERSION}.tar.gz";
+else
+    echo "Unsupported architecture: $ARCH" && exit 1;
+fi
+echo "Runner URL: $RUNNER_URL"
+cd /home/runner
+mkdir actions-runner
+cd actions-runner
+curl -L -O -s $RUNNER_URL
+ls -l
+tar xzf actions-runner-linux-${ARCH}-${RUNNER_VERSION}.tar.gz
+rm actions-runner-linux-${ARCH}-${RUNNER_VERSION}.tar.gz
