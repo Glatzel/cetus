93c0675691065af531118e9a254a1570fafc0602
Glatzel <chenzelin9794@gmail.com>
2026-01-05 13:11:06 +0800

    f

--- images/ghar-linux/build.ps1
+++ images/ghar-linux/build.ps1
@@ -19,9 +19,21 @@ docker buildx build `
     $pushFlag `
     --platform 'linux/amd64,linux/arm64' `
     --build-arg RUNNER_VERSION=$runner_version `
+    --build-arg RUNNER_VERSION=$runner_version `
     --target release-local `
     -t glatzel/ghar-linux-release-local:latest `
     -t glatzel/ghar-linux-release-local:$version `
     -t ghcr.io/glatzel/ghar-linux-release-local:latest `
     -t ghcr.io/glatzel/ghar-linux-release-local:$version `
+    .
+    
+docker buildx build `
+    $pushFlag `
+    --platform 'linux/amd64,linux/arm64' `
+    --build-arg RUNNER_VERSION=$runner_version `
+    --target dev-local `
+    -t glatzel/ghar-linux-dev-local:latest `
+    -t glatzel/ghar-linux-dev-local:$runner_version `
+    -t ghcr.io/glatzel/ghar-linux-dev-local:latest `
+    -t ghcr.io/glatzel/ghar-linux-dev-local:$runner_version `
     .
