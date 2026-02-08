6dbac0a86ffc022541b73f700b92dc926334e352
Glatzel <chenzelin9794@gmail.com>
2026-02-08 11:05:50 +0800

    f

--- images/ghar-win/build.ps1
+++ images/ghar-win/build.ps1
@@ -1,50 +1,34 @@
 $ROOT = git rev-parse --show-toplevel
 . $ROOT/scripts/utils.ps1
 
-$cloud_version = "0.1.0"
-$local_version = "0.0.3"
+$cloud_version = "0.0.1"
+$local_version = "0.0.1"
 $runner_version = "2.331.0"
-$date = "2026-02-04"
+$date = "2026-02-08"
 
-Write-Output "::group::release-cloud"
-docker buildx build `
+Write-Output "::group::cloud"
+docker build `
     $pushFlag `
-    --platform 'linux/amd64,linux/arm64' `
-    --target release-cloud `
-    -t glatzel/ghar-linux-release-cloud:latest `
-    -t glatzel/ghar-linux-release-cloud:v$cloud_version `
-    -t glatzel/ghar-linux-release-cloud:$date `
-    -t ghcr.io/glatzel/ghar-linux-release-cloud:latest `
-    -t ghcr.io/glatzel/ghar-linux-release-cloud:v$cloud_version `
-    -t ghcr.io/glatzel/ghar-linux-release-cloud:$date `
+    --target cloud `
+    -t glatzel/ghar-win-cloud:latest `
+    -t glatzel/ghar-win-cloud:v$cloud_version `
+    -t glatzel/ghar-win-cloud:$date `
+    -t ghcr.io/glatzel/ghar-win-cloud:latest `
+    -t ghcr.io/glatzel/ghar-win-cloud:v$cloud_version `
+    -t ghcr.io/glatzel/ghar-win-cloud:$date `
     .
 Write-Output "::endgroup::"
-Write-Output "::group::release-local"
-docker buildx build `
+Write-Output "::group::local"
+docker build `
     $pushFlag `
-    --platform 'linux/amd64,linux/arm64' `
     --build-arg RUNNER_VERSION=$runner_version `
     --build-arg RUNNER_VERSION=$runner_version `
-    --target release-local `
-    -t glatzel/ghar-linux-release-local:latest `
-    -t "glatzel/ghar-linux-release-local`:v${version}-runner-${runner_version}" `
-    -t "glatzel/ghar-linux-release-local`:${date}" `
-    -t ghcr.io/glatzel/ghar-linux-release-local:latest `
-    -t "ghcr.io/glatzel/ghar-linux-release-local`:v${local_version}-runner-${runner_version}" `
-    -t "ghcr.io/glatzel/ghar-linux-release-local`:${date}" `
-    .
-Write-Output "::endgroup::"
-Write-Output "::group::dev-local"
-docker buildx build `
-    $pushFlag `
-    --platform 'linux/amd64,linux/arm64' `
-    --build-arg RUNNER_VERSION=$runner_version `
-    --target dev-local `
-    -t glatzel/ghar-linux-dev-local:latest `
-    -t "glatzel/ghar-linux-dev-local`:v${version}-runner-${runner_version}" `
-    -t "glatzel/ghar-linux-dev-local`:v${date}" `
-    -t ghcr.io/glatzel/ghar-linux-dev-local:latest `
-    -t "ghcr.io/glatzel/ghar-linux-dev-local`:v${local_version}-runner-${runner_version}" `
-    -t "ghcr.io/glatzel/ghar-linux-dev-local`:v${date}" `
+    --target local `
+    -t glatzel/ghar-win-local:latest `
+    -t "glatzel/ghar-win-local`:v${version}-runner-${runner_version}" `
+    -t "glatzel/ghar-win-local`:${date}" `
+    -t ghcr.io/glatzel/ghar-win-local:latest `
+    -t "ghcr.io/glatzel/ghar-win-local`:v${local_version}-runner-${runner_version}" `
+    -t "ghcr.io/glatzel/ghar-win-local`:${date}" `
     .
 Write-Output "::endgroup::"
