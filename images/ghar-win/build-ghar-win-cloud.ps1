7764af84ea7334b44d7ed76af88dd3981443c25e
Glatzel <893016099@qq.com>
2026-02-09 12:38:14 +0800

    chore: Update build-ghar-win-cloud.ps1

--- images/ghar-win/build-ghar-win-cloud.ps1
+++ images/ghar-win/build-ghar-win-cloud.ps1
@@ -22,7 +22,7 @@ docker image ls
 docker history glatzel/ghar-win-cloud:latest
 if ($env:PUBLISH -eq "true") {
     foreach ($image in $images) {
-        foreach ($tag in $tags | Where-Object { $_ -ne "ltsc2025" }) {
+        foreach ($tag in $tags) {
             docker push "$image`:$tag"
         }
     }
