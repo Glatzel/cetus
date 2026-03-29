2da7104e19cb1f554a6b45928f1879076ba419dc
Glatzel <893016099@qq.com>
2026-03-29 17:15:29 +0800

    Update start-runner.sh

--- images/ghar-linux-dev-local/start-runner.sh
+++ images/ghar-linux-dev-local/start-runner.sh
@@ -10,8 +10,6 @@ RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/runner/actions-runner
-
 /home/runner/.pixi/envs/runner/config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
