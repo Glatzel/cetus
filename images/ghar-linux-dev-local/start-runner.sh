06911e97d9fac046e713d5fb7ab937cdc84b06ec
Glatzel <893016099@qq.com>
2026-03-29 17:22:55 +0800

    Update start-runner.sh

--- images/ghar-linux-dev-local/start-runner.sh
+++ images/ghar-linux-dev-local/start-runner.sh
@@ -9,15 +9,15 @@ RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
 RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
 
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
-
-/home/runner/.pixi/envs/runner/config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
+cd /home/.pixi/envs/runner
+./config.sh --unattended --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
     echo "Removing runner..."
-    /home/runner/.pixi/envs/runner/config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove --unattended --token ${REG_TOKEN}
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-/home/runner/.pixi/envs/runner/run.sh & wait $!
+./run.sh & wait $!
