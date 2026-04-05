e94497067b1db84e5b28d914c60f12465a9f8687
Glatzel <893016099@qq.com>
2026-04-05 16:43:07 +0800

    Update start-runner.sh

--- images/ghar-linux-dev/start-runner.sh
+++ images/ghar-linux-dev/start-runner.sh
@@ -47,4 +47,10 @@ cleanup() {
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 log "Starting GitHub Actions runner..."
-./run.sh & wait $!
+if [ "${EPHEMERAL}" = "true" ]; then
+    ./run.sh
+    log "Ephemeral runner finished job, exiting..."
+    exit 0
+else
+    ./run.sh & wait $!
+fi
